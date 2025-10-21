0660e2bd739d8344bb109dd4370272167412d56e
Ali Hosseinkhani <aliho3einkhani1@gmail.com>
2025-10-21 11:48:30 +0330

    refactor

--- src/main.js
+++ src/main.js
@@ -6,17 +6,36 @@ const scene = new three.Scene()
 
 // creating cube
 const cubeGeometry = new three.BoxGeometry(1, 1, 1)
-const cubeMaterial = new three.MeshBasicMaterial({ color: 'purple' })
+const cubeMaterial = new three.MeshBasicMaterial({ color: 'purple', wireframe: true })
 const cubeMesh = new three.Mesh(
   cubeGeometry,
   cubeMaterial
 )
-scene.add(cubeMesh)
+cubeMesh.rotation.y = three.MathUtils.degToRad(45)
+// cubeMesh.rotation.reorder('XYZ')
 
-cubeMesh.position.y = 0
+const cubeMesh2 = new three.Mesh(
+  cubeGeometry,
+  cubeMaterial
+)
+cubeMesh2.position.x = 2 // relative to the grp
+
+const cubeMesh3 = new three.Mesh(
+  cubeGeometry,
+  cubeMaterial
+)
+cubeMesh3.position.x = -2 // relative to the grp
+
+const group = new three.Group()
+group.add(cubeMesh, cubeMesh2, cubeMesh3)
+group.scale.y = 2 // applies to all children
+group.position.y = 2 // applies to all children
+scene.add(cubeMesh)
+scene.add(group)
+
 
 //axes
-const axesHelper = new three.AxesHelper(2)
+const axesHelper = new three.AxesHelper(3)
 scene.add(axesHelper)
 
 // creating camera
@@ -26,8 +45,10 @@ const camera = new three.PerspectiveCamera(
   0.5,
   30
 )
-camera.position.z = 5
+camera.position.z = 10
 scene.add(camera)
+
+const distance = cubeMesh.position.distanceTo(camera.position)
 
 //creating render
 const canvas = document.querySelector('.three')
@@ -37,7 +58,7 @@ const render = new three.WebGLRenderer(
 
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true
-controls.autoRotate = true
+// controls.autoRotate = true
 render.setSize(window.innerWidth, window.innerHeight)
 // const maxPixelRatio = Math.min(window.devicePixelRatio, 2)
 // render.setPixelRatio(maxPixelRatio)
