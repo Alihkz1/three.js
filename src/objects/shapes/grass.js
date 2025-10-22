4c2b06de52509ce962d9c78785960619718395cc
Ali Hosseinkhani <aliho3einkhani1@gmail.com>
2025-10-22 15:47:24 +0330

    change texture offset with pane

--- src/objects/shapes/grass.js
+++ src/objects/shapes/grass.js
@@ -1,14 +1,32 @@
 import * as three from 'three'
+import { Pane } from 'tweakpane'
 
+const pane = new Pane()
 const textureLoader = new three.TextureLoader()
+
+//grass texture
 const grass = textureLoader.load('/panel1.png')
 grass.repeat.set(2, 2)
 grass.wrapS = three.RepeatWrapping // x
 grass.wrapT = three.RepeatWrapping // y
-grass.offset.x = 0.5
+// grass.offset.x = 0.5
+pane.addInput(grass, "offset", {
+    x: {
+        min: -1,
+        max: 1,
+        step: 0.001
+    },
+    y: {
+        min: -1,
+        max: 1,
+        step: 0.001
+    }
+})
 
-const material = new three.MeshBasicMaterial({ side: three.DoubleSide })
+const material = new three.MeshStandardMaterial({ side: three.DoubleSide })
 material.map = grass
+// material.roughnessMap = png 
+// material.metalness = 1 
 
 const geometry = new three.PlaneGeometry(1, 1)
 
